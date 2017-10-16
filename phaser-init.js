dd6cf13255ce7f58b9f6333335d5655ee2c70dc0
Becky Rush <rushlet@users.noreply.github.com>
2017-10-16 20:45:51 +0100

    add a baddie
    baddie 3 chances collision needs improving / throttling

--- phaser-init.js
+++ phaser-init.js
@@ -3,12 +3,15 @@ function init() {
     var platforms;
     var score = 0;
     var scoreText;
+    var baddieHitCount = 0;
+    var gameOver;
 
     function preload() {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('star', 'assets/star.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
     }
 
     function create() {
@@ -22,11 +25,14 @@ function init() {
         game.physics.arcade.enable(player);
         stars = game.add.group();
         stars.enableBody = true;
-        
+        baddie = game.add.sprite(game.world.width - 60, game.world.height - 100, 'baddie');
+        game.physics.arcade.enable(baddie);
+
         createGroundAndLedges(platforms);
         createPlayer(player);
         createStars(stars);
-        
+        createBaddie(baddie);
+
 
         scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
     }
@@ -34,13 +40,26 @@ function init() {
     function update() {
         // stars hit the platforms
         game.physics.arcade.collide(stars, platforms);
+        // make baddie land on ground
+        game.physics.arcade.collide(baddie, platforms);
         //  Collide the player and the stars with the platforms
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         // check if player overlaps with star
         game.physics.arcade.overlap(player, stars, collectStar, null, this);
+        game.physics.arcade.overlap(player, baddie, hitBaddie, null, this);
         movePlayer(hitPlatform);
+
+
+        if (baddie.world.x <= 0) {
+          baddie.body.velocity.x = (Math.floor(Math.random() * 51) + 120);
+          baddie.animations.play('right');
+       }
+       else if (baddie.world.x >= game.world.width - 32) {
+         baddie.body.velocity.x = -(Math.floor(Math.random() * 51) + 100);
+         baddie.animations.play('left');
+       }
     }
-    
+
     function createGroundAndLedges() {
         // Here we create the ground.
         var ground = platforms.create(0, game.world.height - 32, 'ground');
@@ -64,12 +83,23 @@ function init() {
         player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
 
+    function createBaddie(baddie) {
+         // The player and its settings
+        baddie.body.bounce.y = 0.1;
+        baddie.body.gravity.y = 300 ;
+        baddie.body.collideWorldBounds = true;
+        baddie.animations.add('left', [0, 1], 10, true);
+        baddie.animations.add('right', [2, 3], 10, true);
+        baddie.body.velocity.x = -150;
+        baddie.animations.play('left');
+    }
+
     function createStars(stars) {
         //  Here we'll create 12 of them evenly spaced apart
         for (var i = 0; i < 12; i++)
         {
             //  Create a star inside of the 'stars' group
-            var star = stars.create(i * (Math.floor(Math.random() * 35) + 20), 0, 'star');
+            var star = stars.create(i * 30, 0, 'star');
             star.scale.x = 0.2;
             star.scale.y = 0.2;
             //  Let gravity do its thing
@@ -109,14 +139,23 @@ function init() {
     function collectStar (player, star) {
         // Removes the star from the screen
         star.kill();
-        
         //  Add and update the score
         score += 10;
         scoreText.text = 'Score: ' + score;
+    }
+
+    function hitBaddie (player, baddie) {
+      if (baddieHitCount < 3) {
+        score -= 10;
+        scoreText.text = 'Score: ' + score;
+        baddieHitCount ++;
+      } else {
+        player.kill();
+        gameOver = game.add.text(game.world.width / 2 - 15, game.world.height / 2, 'GAME OVER', { fontSize: '32px', fill: '#000' });
+      }
     }
 }
 
 window.onload = function() {
   init();
 }
-
