988660dc8b3ecbc71531ac897dd5cacced799c44
Becky Rush <rushlet@users.noreply.github.com>
2017-10-16 16:04:00 +0100

    add platforms and ground and more assets

--- phaser-init.js
+++ phaser-init.js
@@ -1,9 +1,13 @@
 function init() {
     var game = new Phaser.Game(360, 600, Phaser.AUTO, 'gameWindow', { preload: preload, create: create, update: update });
+    
+    var platforms;
 
     function preload() {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('star', 'assets/star.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
     function create() {
@@ -13,6 +17,33 @@ function init() {
         var star = game.add.sprite(0, 0, 'star');
         star.scale.x = 0.5;
         star.scale.y = 0.5;
+        
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
+
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
+
+        // Here we create the ground.
+        var ground = platforms.create(0, game.world.height - 32, 'ground');
+
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
+
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
+
+        //  Now let's create two ledges
+        var ledge = platforms.create(230, 475, 'ground');
+        ledge.scale.x = 0.2;
+        
+        ledge.body.immovable = true;
+
+        ledge = platforms.create(10, 300, 'ground');
+        ledge.scale.x = 0.3;
+
+
+        ledge.body.immovable = true;    
     }
 
     function update() {
